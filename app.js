fe657021687fda448dec60898dbcfcba950b0bdb
Angelogmc <angelothiagocosta@gmail.com>
2023-05-05 22:06:14 -0300

    changes

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const pokemon_result = document.getElementById(pokemon_result);
-const pokemon_button = document.getElementById(pokemon_button);
+const pokemon_btn = document.getElementById(pokemon_btn);
 
-pokemon_button.addEventListener('click', getPokemon);
+pokemon_button.addEventListener('click', getRandomDog);
 
-function getPokemon() {
-    fetch('https://randomfox.ca/floof/')
-       .then(res => res.json())
-       .then(data => {
-           pokemon_result.innerHTML = <img src= {data.image}/>
-    })
+function getRandomDog() {
+	fetch('https://random.dog/woof.json')
+		.then(res => res.json())
+		.then(data => {
+			if(data.url.includes('.mp4')) {
+				getRandomDog();
+			}
+			else {
+				pokemon_result.innerHTML = `<img src=${data.url} alt="dog" />`;
+			}
+		});
 }
